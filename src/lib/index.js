d49713cbab1c34a753b453fa3d176f03820c86fb
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-11-01 09:20:45 -0600

    move ScenarioParser into lib folder

---
+++ src/lib/index.js
@@ -0,0 +1,1 @@
+export { default as ScenarioParser } from "./ScenarioParser";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { AssignmentsContext, PeopleContext, ScenariosContext } from "contexts";
 import { ScenariosTimeline } from "components";
 import { useForecast, useLocalStorage, usePipedrive } from "hooks";
+import { ScenarioParser } from "lib";
 
 const DEALS_STORAGE_KEY = "deals";
 const PROJECTS_STORAGE_KEY = "projects";
@@ -53,48 +54,3 @@ export default function Home() {
   let possibleScenarios = ScenarioParser(upcomingScenarios, allAssignments, allPeople, projects, deals);
   return <ScenariosTimeline events={[...nowScenarios, ...possibleScenarios]} people={[allPeople]} />;
 }
-
-const ScenarioParser = (scenarios, assignments, people, projects, deals) => {
-  let projectIds = [...new Set(assignments.map((a) => a.projectId))];
-  let parsedProjects = projectIds.map((id) => projects.find((p) => p.id === id)).filter((p) => !!p);
-  let parsedDeals = projectIds.map((id) => deals.find((d) => d.id === id)).filter((d) => !!d);
-  return scenarios.map((scenario) => {
-    let staffedDeals = parsedDeals.filter((p) =>
-      assignments.find((a) => a.scenarioId === scenario.id && a.projectId === p.id)
-    );
-    let staffedProjects = [...parsedProjects, ...staffedDeals];
-
-    return {
-      id: scenario.id,
-      title: scenario.name,
-      projects: staffedProjects.map((project) => {
-        const projectAssignments = assignments.filter((a) => {
-          let bool = a.projectId === project.id;
-          if (!!a.scenarioId) {
-            bool = bool && a.scenarioId === scenario.id;
-          }
-          return bool;
-        });
-
-        const projectStart = new Date(Math.min(...projectAssignments.map((p) => p.startDate)));
-        const projectEnd = new Date(Math.max(...projectAssignments.map((p) => p.endDate)));
-
-        const staffedPeople = projectAssignments.map((a) => {
-          let person = people.find((p) => a.personId === p.id);
-          return {
-            ...person,
-            assignment: { ...a },
-          };
-        });
-
-        return {
-          id: project.id,
-          name: project.name,
-          startDate: projectStart,
-          endDate: projectEnd,
-          people: staffedPeople,
-        };
-      }),
-    };
-  });
-};
