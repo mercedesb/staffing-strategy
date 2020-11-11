51316964dca871b23bcc812172a9d21cae143851
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-11-11 10:41:00 -0600

    fix console warning

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Input } from "reakit/Input";
 
 export function TextInput(props) {
-  const { label, inputRef } = props;
+  const { label, inputRef, ...otherProps } = props;
 
   return (
     <div className="pb-4">
       <label className={`w-full flex flex-col ${props.disabled ? "text-tandemDarkGray" : ""}`}>
         {label}
         <Input
-          {...props}
+          {...otherProps}
           className="border-tandemMdGray border-b border-t-0 border-l-0 border-r-0 mb-2 leading-4 p-2"
           style={{ height: "2.5rem" }}
           ref={inputRef}
