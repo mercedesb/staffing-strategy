bcd59081f103f71bc8504f3e51bc98ff3b5d73d9
Nora Harris <noragharris@gmail.com>
2021-04-09 12:51:54 -0600

    eslint on TextInput.test

--- src/components/__tests__/TextInput.test.js
+++ src/components/__tests__/TextInput.test.js
@@ -1,26 +1,21 @@
-import React from "react"
-import { TextInput } from "../TextInput"
-import { render, screen } from "@testing-library/react"
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { TextInput } from '../TextInput';
 
+describe('TextInput', () => {
+  const lableName = 'Name';
+  it('is grayed out when disabled', () => {
+    render(<TextInput label={lableName} disabled={true} />);
 
-describe("TextInput", () => {
-  let lableName = "Name"
-  it("is grayed out when disabled", () => {
-    render(
-      <TextInput label={lableName} disabled={true} />
-    )
+    const component = screen.getByText(lableName);
 
-    const component = screen.getByText(lableName)
+    expect(component).toHaveClass('text-tandemDarkGray');
+  });
+  it('is not grayed out when not disabled', () => {
+    render(<TextInput label={lableName} disabled={false} />);
 
-    expect(component).toHaveClass('text-tandemDarkGray')
-  })
-  it("is not grayed out when not disabled", () => {
-    render(
-      <TextInput label={lableName} disabled={false} />
-    )
+    const component = screen.getByText(lableName);
 
-    const component = screen.getByText(lableName)
-
-    expect(component).not.toHaveClass('text-tandemDarkGray')
-  })
-})
+    expect(component).not.toHaveClass('text-tandemDarkGray');
+  });
+});
